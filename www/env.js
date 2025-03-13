1f1383b0f93c5101b98cf0a93c95accd30a703f4
ethanrgibson <ethanrgibson33@gmail.com>
2025-03-13 14:30:47 -0600

    auth working

--- www/env.js
+++ www/env.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-d3qj26bqdtixpiu0.us.auth0.com'
+export const clientId = 'rX26uYnOTmtdPnaKkWDK8b8KiA1HbTLi'
+export const audience = 'https://auth.optamasticprimefoundation.com'
