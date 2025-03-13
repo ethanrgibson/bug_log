23a8387d58c121542a36f3c8524a236772237999
ethanrgibson <ethanrgibson33@gmail.com>
2025-03-13 14:38:33 -0600

    bug model and db context created

---
+++ src/models/Bug.js
@@ -0,0 +1,22 @@
+import { Schema } from "mongoose";
+
+export const BugSchema = new Schema(
+  {
+
+    title: { type: String, required: true, minLength: 10, maxLength: 50 },
+    description: { type: String, required: true, minLength: 10, maxLength: 500 },
+    priority: { type: Number, required: true, min: 1, max: 5 },
+    closed: { type: Boolean, required: true, default: false },
+    closedDate: { type: Date },
+    creatorId: { type: Schema.ObjectId, ref: 'Account' }
+
+  },
+
+  {
+    timestamps: true,
+    toJSON: { virtuals: true }
+
+  }
+)
+
+
